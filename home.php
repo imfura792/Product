3728b92b456755b2e735fa17695165d36e94d76a
imfura792 <83733630+imfura792@users.noreply.github.com>
2024-02-16 03:44:32 -0800

    Update home.php

--- home.php
+++ home.php
@@ -52,7 +52,7 @@ $totalProductsPerSupplierData = $totalProductsPerSupplierStmt->fetchAll(PDO::FET
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Product Management System</title>
+    <title>Product Management Style</title>
 
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css">
